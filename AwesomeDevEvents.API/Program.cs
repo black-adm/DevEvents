d8f374c2c2df3931463d68d0ec406169609e5061
black-adm <mattheuscontato17@gmail.com>
2023-09-26 12:58:09 -0300

    Update config for add Swagger Documentation

--- AwesomeDevEvents.API/Program.cs
+++ AwesomeDevEvents.API/Program.cs
@@ -1,5 +1,6 @@
 using AwesomeDevEvents.API.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DevEventsCs");
@@ -7,7 +8,24 @@ var connectionString = builder.Configuration.GetConnectionString("DevEventsCs");
 builder.Services.AddDbContext<DevEventDbContext>(o => o.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(a =>
+{
+    a.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "DevEvents.API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Matheus Roberto Madureira",
+            Email = "matheus.dotnet@outlook.com",
+            Url = new Uri("https://br.linkedin.com/in/matheus-roberto-240726292")
+        }
+    });
+
+    var xmlFile = "DevEvents.API.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    a.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
